2bb63ec72ee70f1c82a8779819b8d23b675a0b0c
Josh Williams <josh@spacesnottabs.com>
2022-08-14 07:38:41 +0000

    Moved http_adapter to register common

--- lib/register_ingester_psc/config/adapters.rb
+++ lib/register_ingester_psc/config/adapters.rb
@@ -1,12 +1,12 @@
+require 'register_common/adapters/http_adapter'
 require 'register_common/adapters/s3_adapter'
 require 'register_ingester_psc/config/settings'
-require 'register_ingester_psc/adapters/http_adapter'
 
 module RegisterIngesterPsc
   module Config
     module Adapters
+      HTTP_ADAPTER = RegisterCommon::Adapters::HttpAdapter.new
       S3_ADAPTER = RegisterCommon::Adapters::S3Adapter.new(credentials: AWS_CREDENTIALS)
-      HTTP_ADAPTER = RegisterIngesterPsc::Adapters::HttpAdapter.new
     end
   end
 end
