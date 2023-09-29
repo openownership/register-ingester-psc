11ad28f4792c2d18e13c969e3a82bb20c8f896ce
Nicolas Williams <tp@tiredpixel.com>
2023-09-29 17:23:40 +0200

    [#188] refactor CI to use Docker image

--- lib/register_ingester_psc/config/settings.rb
+++ lib/register_ingester_psc/config/settings.rb
@@ -1,11 +1,3 @@
-require 'dotenv'
-
-if ENV['TEST'].to_i == 1
-  Dotenv.load('.test.env')
-else
-  Dotenv.load('.env')
-end
-
 module RegisterIngesterPsc
   module Config
     AwsCredentialsStruct = Struct.new(
